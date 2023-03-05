7a8904f750da11bd0ecf7bf7d6c5dbe6d2bf46bd
root <Melvin.Kumar@radisys.com>
2023-03-06 00:11:11 +0530

    improved the ui of our application snippetbox

--- snippetbox/cmd/web/handler.go
+++ snippetbox/cmd/web/handler.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// check if the current request URL path exactly matches with "/". If it does not matches, we are sending 404 page not found error to the client.
 	// importantly return after http.NotFound() otherwise function will keep wexecuting till the end.
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from snippetbox"))
+
+	files := []string{
+		"ui/html/home.page.tmpl",
+		"ui/html/base.layout.tmpl",
+		"ui/html/footer.partial.tmpl",
+	}
+	temp, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Println(err)
+		http.Error(w, "internal server error", 500)
+		return
+	}
+
+	err = temp.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", 500)
+		return
+	}
+
 }
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	fmt.Fprintf(w, "Display a specific snippet with ID %d.......", id)
@@ -27,7 +48,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display as specific snippet...."))
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"net"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
 func main() {
-	serverMux := http.NewServeMux()
+	addr := flag.String("addr", ":8085", "HTTP network address")
+	flag.Parse()
 
-	serverMux.HandleFunc("/", home)
-	serverMux.HandleFunc("/snippet", showSnippet)
-	serverMux.HandleFunc("/snippet/create", createSnippet)
-
-	con, err := net.Listen("tcp", ":8085")
-	if err != nil {
-		panic(err)
+	// creating custom infoLog and errorLog
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	mux := http.NewServeMux()
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
 	}
-	log.Println("starting server on port 8085")
-	if err := http.Serve(con, serverMux); err != nil {
-		panic(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileserver := http.FileServer(http.Dir("ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	sv := http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("starting server on address: %s", *addr)
+	if err := sv.ListenAndServe(); err != nil {
+		errorLog.Panic(err)
 	}
 
 }
