02118960175a819bce4a809e91609336ae32ce57
money <moneymanav9211@gmail.com>
2023-04-16 01:47:14 +0530

    updated routes

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,14 +7,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Prasang-money/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
 type application struct {
 	infoLog       *log.Logger
 	errorLog      *log.Logger
+	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
@@ -23,6 +26,7 @@ func main() {
 	addr := flag.String("addr", ":8085", "HTTP network address")
 	//web:pass@/snippetbox?parseTime=true
 	dsn := flag.String("dsn", "root:money@/snippetbox?parseTime=true", "MySQL databse  connection string")
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key used to encrypt and authenticate session cookies")
 	flag.Parse()
 	// creating custom infoLog and errorLog
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -39,10 +43,17 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+
+	// Use the sessions.New() function to initialize a new session manager,
+	// passing in the secret key as the parameter. Then we configure it so
+	// sessions always expires after 12 hours.
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
 	// initialize the SnippetModel object and add it into the application dependency
 	app := &application{
 		infoLog:       infoLog,
 		errorLog:      errorLog,
+		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
